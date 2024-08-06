85ddb67460fcc0a69a5f90e4f882f5426db93c05
SenpaiSumpie <max.lee.mosier@gmail.com>
2024-08-05 22:31:08 -0500

    removing auth pages

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignInForm from '@/modules/authentication/components/sign-in-form';
-
-export default function Page() {
-    return (
-        <div className="w-full flex justify-center pt-8 pb-8">
-            <SignInForm />
-        </div>
-    );
-}
--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import SignUpForm from '@/modules/authentication/components/sign-up-form';
-
-export default function Page() {
-    return (
-        <div className="w-full flex justify-center pt-8 pb-8">
-            <SignUpForm />
-        </div>
-    );
-}
